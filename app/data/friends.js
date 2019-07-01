735ed7e4d06cf8de620ae2905755d00f2ebc40b3
Dominiquenw <domnwright18@gmail.com>
2019-07-01 18:27:17 -0400

    needs improvement

---
+++ app/data/friends.js
@@ -0,0 +1,104 @@
+// All the possible friend matches 
+
+var friends = [
+    {
+      name: "Diosa",
+      photo: "https://i.pinimg.com/originals/1e/6f/b3/1e6fb31c6f5ae547253f4515cd15c9ff.jpg",
+      scores: [
+        "1",
+        "1",
+        "5",
+        "3",
+        "3",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "King",
+      photo: "https://i.pinimg.com/736x/0e/fd/59/0efd59b5af084d8cacf5eb7d685e356d.jpg",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "2",
+        "3",
+        "2",
+        "4",
+        "1",
+        "4",
+        "2"
+      ]
+    },
+    {
+      name: "Nola Darling",
+      photo: "https://res.cloudinary.com/blavity/image/upload/c_crop,g_center,w_auto,q_auto:best,g_south_east,x_0/v1513264551/dg59vn2i7icqqp8mtwm2",
+      scores: [
+        "5",
+        "2",
+        "3",
+        "2",
+        "5",
+        "2",
+        "3",
+        "3",
+        "3",
+        "5"
+      ]
+    },
+    {
+      name: "Issa Rae",
+      photo: "https://si.wsj.net/public/resources/images/BN-PY335_wolfe_FR_20160922145642.jpg",
+      scores: [
+        "4",
+        "3",
+        "3",
+        "2",
+        "1",
+        "1",
+        "5",
+        "2",
+        "4",
+        "3"
+      ]
+    },
+    {
+      name: "Khaleesi",
+      photo: "https://cdn5.newsnation.in/images/2019/03/22/emilie-121918783_6.jpg",
+      scores: [
+        "2",
+        "3",
+        "3",
+        "1",
+        "5",
+        "4",
+        "5",
+        "2",
+        "1",
+        "3"
+      ]
+    },
+    {
+      name: "Chun Li",
+      photo: "https://i.kinja-img.com/gawker-media/image/upload/s--0PO_Vjd7--/c_scale,f_auto,fl_progressive,q_80,w_800/j7xbe1avxea3i1bmdcff.jpg",
+      scores: [
+        "2",
+        "4",
+        "2",
+        "3",
+        "1",
+        "2",
+        "4",
+        "3",
+        "2",
+        "5"
+      ]
+    }
+  
+  ];
+  
+  
+  
